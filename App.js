6f9d2657e8d43639fd8d0d377ab542e8636d0d73
Hilmi Güner <hilmi.guner@hotmail.com>
2023-12-01 20:39:13 +0300

    GestureHandler has been installed and GestureHandlerRootView has been added to App.js as a root wrapper

--- App.js
+++ App.js
@@ -2,29 +2,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './screens/MainScreen';
 import RobotControlScreen from './screens/RobotControlScreen';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   const MainScreenStack = createNativeStackNavigator();
   return(
-    <NavigationContainer>
-      <MainScreenStack.Navigator>
-        <MainScreenStack.Screen
-          name='MainScreen'
-          component={MainScreen}
-          options={{
-            title: "Agri Sense Tech"
-          }}
-        />
-        <MainScreenStack.Screen
-          name='RobotControlScreen'
-          component={RobotControlScreen}
-          options={{
-            title: "Robot Control"
-          }}
-        />
-      </MainScreenStack.Navigator>
-    </NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1,}}>
+        <NavigationContainer>
+          <MainScreenStack.Navigator>
+            <MainScreenStack.Screen
+              name='MainScreen'
+              component={MainScreen}
+              options={{
+                title: "Agri Sense Tech"
+              }}
+            />
+            <MainScreenStack.Screen
+              name='RobotControlScreen'
+              component={RobotControlScreen}
+              options={{
+                title: "Robot Control"
+              }}
+            />
+          </MainScreenStack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
   );
 }
 
