6a64400b40d0ea3eb08fccef2e9678477fe6c820
Hilmi Güner <hilmi.guner@hotmail.com>
2023-12-02 07:05:59 +0300

    VirtualJoystick has been created

---
+++ components/VirtualJoystick.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+
+function VirtualJoystick({ padBackgroundColor, padBackgroundOpacity, padPointColor, padOuterCircleSize, padInnerCircleSize }) {
+    const centerPointPos = (padOuterCircleSize-padInnerCircleSize)/2;
+    const [xPos, setX_Pos] = useState(centerPointPos);
+    const [yPos, setY_Pos] = useState(centerPointPos);
+
+    let bgColor = padBackgroundColor;
+    if(padBackgroundOpacity != 1) {
+        bgColor = padBackgroundColor + (100*padBackgroundOpacity).toString().slice(0, 2);
+    }
+    const pan = Gesture.Pan()
+    .onUpdate((event) => {
+        let newX;
+        let newY;
+        let degree = Math.atan(event.translationY/event.translationX);
+        if(isNaN(degree)) {
+            if(event.translationY < 0) {
+                degree = 1.57079633;
+            }
+            else {
+                degree = -1.57079633
+            }
+        }
+        let yDelta = (padOuterCircleSize/2)*Math.sin(degree);
+        let xDelta = (padOuterCircleSize/2)*Math.cos(degree);
+        if(
+            (event.translationX < 0 && event.translationY < 0) 
+            || 
+            (event.translationX < 0 && event.translationY > 0) 
+        ) {
+            xDelta = -xDelta;
+            yDelta = -yDelta;
+        }
+        if(event.translationY == 0) {
+            if(event.translationX > 0) {
+                xDelta = Math.abs(xDelta);
+            }
+            else {
+                xDelta = -Math.abs(xDelta);
+            }
+        }
+        if(event.translationX == 0) {
+            if(event.translationY > 0) {
+                yDelta = Math.abs(yDelta);
+            }
+            else {
+                yDelta = -Math.abs(yDelta);
+            }
+        }
+        newX = (centerPointPos + xDelta);
+        newY = (centerPointPos + yDelta);
+        if(Math.abs(event.translationX) < Math.abs(xDelta)) {
+            newX = (centerPointPos + event.translationX);
+        }
+        if(Math.abs(event.translationY) < Math.abs(yDelta)) {
+            newY = (centerPointPos + event.translationY);
+        }
+
+        setX_Pos(Math.floor(newX));
+        setY_Pos(Math.floor(newY));
+        console.log(`x force: ${event.translationX}, y force: ${event.translationY}`);
+    })
+    .onEnd((_) => {
+        setX_Pos(centerPointPos);
+        setY_Pos(centerPointPos);
+    });
+    return(
+        <View style={[styles.padBoundary, {
+            backgroundColor: bgColor,
+            width: padOuterCircleSize, 
+            height: padOuterCircleSize, 
+            borderRadius: (padOuterCircleSize/2)
+        }]}>
+            <GestureDetector gesture={pan}>
+                    <Animated.View style={[styles.padPoint, {
+                        backgroundColor: padPointColor,
+                        width: padInnerCircleSize,
+                        height: padInnerCircleSize,
+                        borderRadius: (padInnerCircleSize/2),
+                        top: yPos,
+                        left: xPos,
+                    }]}>
+                    </Animated.View>
+            </GestureDetector>
+        </View>
+    );
+}
+
+export default VirtualJoystick;
+
+const styles = StyleSheet.create({
+    rootContainer: {},
+    padBoundary: {},
+    padPoint: {
+        position: "relative",
+    },
+});
--- screens/RobotControlScreen.js
+++ screens/RobotControlScreen.js
@@ -1,11 +1,26 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import VirtualJoystick from "../components/VirtualJoystick";
 
 function RobotControlScreen() {
     return(
-        <View>
-
+        <View style={styles.rootContainer}>
+            <VirtualJoystick 
+                padBackgroundColor={"#f50713"} 
+                padBackgroundOpacity={0.6} 
+                padPointColor={"blue"}
+                padOuterCircleSize={100}
+                padInnerCircleSize={35}
+            />
         </View>
     );
 }
 
 export default RobotControlScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
