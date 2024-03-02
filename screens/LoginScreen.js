d107b46ad7ad4b993bf06c2cb1aaed547a97a7ca
Hilmi Güner <hilmi.guner@hotmail.com>
2024-03-02 14:10:30 +0300

    Prototype of the login screen is ready.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,42 +1,45 @@
-import { Pressable, Text, View } from "react-native";
+import { Dimensions, Pressable, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { ImageBackground } from "react-native";
 
 function LoginScreen() {
     const safeAreaInsets = useSafeAreaInsets();
-
     return(
-        <View style={{ flex: 1, justifyContent: "space-between", paddingTop: safeAreaInsets.top }}>
+        <ImageBackground
+            source={require("../assets/images/background.jpg")}
+            style={{ flex: 1, paddingTop: safeAreaInsets.top, paddingBottom: safeAreaInsets.bottom, justifyContent: "space-between" }}
+        >
             <View>
                 <Text style={{ textAlign: "center", marginVertical: 32, fontSize: 24, fontWeight: "bold"}}>Login</Text>
-                <View style={{ flexDirection: "row", backgroundColor: "red", marginHorizontal: 32, borderRadius: 24, padding: 12, marginBottom: 12 }}>
+                <View style={{ flexDirection: "row", backgroundColor: "#f1f1f1", marginHorizontal: 32, borderRadius: 24, padding: 12, marginBottom: 12 }}>
                     <Icon name={"person"} size={24} color={"black"}/>
                     <TextInput style={{ marginLeft: 8, padding: 0 }} placeholder="Username"/>
                 </View>
-                <View style={{ flexDirection: "row", backgroundColor: "red", marginHorizontal: 32, borderRadius: 24, padding: 12, marginBottom: 48 }}>
+                <View style={{ flexDirection: "row", backgroundColor: "#f1f1f1", marginHorizontal: 32, borderRadius: 24, padding: 12, marginBottom: 48 }}>
                     <Icon name={"lock"} size={24} color={"black"}/>
                     <TextInput style={{ marginLeft: 8, padding: 0 }} secureTextEntry={true} placeholder="Password"/>
                 </View>
-                <Pressable style={{ backgroundColor: "red", marginHorizontal: 32, borderRadius: 24, padding: 12, marginBottom: 12 }}>
+                <Pressable style={{ backgroundColor: "#f1f1f1", marginHorizontal: 32, borderRadius: 24, padding: 12, marginBottom: 12 }}>
                     <Text style={{ textAlign: "center"}}>Login</Text>
                 </Pressable>
                 <Pressable>
                     <Text style={{ textAlign: "center"}}>Forgot your password?</Text>
                 </Pressable>
             </View>
-            <View style={{ marginBottom: 48 }}>
+            <View>
                 <View style={{ flexDirection: 'row', alignItems: 'center', padding: 12, }}>
-                    <View style={{ flex: 1, height: 1, backgroundColor: 'red' }} />
+                    <View style={{ flex: 1, height: 1, backgroundColor: '#f1f1f1' }} />
                     <Text style={{ textAlign: 'center', marginHorizontal: 8 }}>or connect with</Text>
-                    <View style={{ flex: 1, height: 1, backgroundColor: 'red' }} />
+                    <View style={{ flex: 1, height: 1, backgroundColor: '#f1f1f1' }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Pressable style={{ flex: 1, backgroundColor: "red", marginHorizontal: 12, padding: 8, borderRadius: 24 }}>
+                    <Pressable style={{ flex: 1, backgroundColor: "#f1f1f1", marginHorizontal: 12, padding: 8, borderRadius: 24 }}>
                         {/* Icon */}
                         <Text style={{ textAlign: "center" }}>Facebook</Text>
                     </Pressable>
-                    <Pressable style={{ flex: 1, backgroundColor: "red", marginHorizontal: 12, padding: 8, borderRadius: 24 }}>
+                    <Pressable style={{ flex: 1, backgroundColor: "#f1f1f1", marginHorizontal: 12, padding: 8, borderRadius: 24 }}>
                         {/* Icon */}
                         <Text style={{ textAlign: "center" }}>Google</Text>
                     </Pressable>
@@ -48,7 +51,7 @@ function LoginScreen() {
                     </Pressable>
                 </View>
             </View>
-        </View>
+        </ImageBackground>
     );
 }
 
