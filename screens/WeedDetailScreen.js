49ebdf8877d6010774b7d1b699769680957e222b
Hilmi Güner <hilmi.guner@hotmail.com>
2024-05-31 01:46:32 +0300

    Image viewer added to app

--- screens/WeedDetailScreen.js
+++ screens/WeedDetailScreen.js
@@ -1,13 +1,16 @@
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import IconButton from "../components/ui/IconButton";
 import theme from "../util/theme";
 import FastImage from "react-native-fast-image";
 import StandardButton from "../components/ui/StandardButton";
+import ImageView from "react-native-image-viewing";
+import { useState } from "react";
 
 const screenSize = Dimensions.get("screen");
 
 function WeedDetailScreen({ navigation, route }) {
+    const [visible, setVisible] = useState(false);
     const safeAreaInsets = useSafeAreaInsets();
     // Keys for data => ["id", "fb_local_id", "image", "image_path", "latitude", "longitude", "percentage"]
     const data = route.params.data;
@@ -16,14 +19,20 @@ function WeedDetailScreen({ navigation, route }) {
     
     return(
         <View style={[styles.root, { paddingTop: safeAreaInsets.top, paddingBottom: safeAreaInsets.bottom }]}>
+            <ImageView
+                images={[{ uri: imageUri }]}
+                imageIndex={0}
+                visible={visible}
+                onRequestClose={() => setVisible(false)}
+            />
             <View style={{ margin: 12 }}>
                 <IconButton color={"white"} icon={"chevron-back"} iconBundle={"Ionicons"} size={24} onPress={navigation.goBack}/>
             </View>
             <View style={{ flex: 1, justifyContent: "space-between"}}>
                 <View>
-                    <View>
+                    <Pressable onPress={() => setVisible(true)}>
                         <FastImage source={{ uri: imageUri }} style={styles.image}/>
-                    </View>
+                    </Pressable>
                     <View style={{ margin: 12 }}>
                         <Text style={styles.textBold}>Weed</Text>
                         <Text style={styles.text}>Percentage: <Text style={styles.textBold}>{data.percentage}</Text></Text>
